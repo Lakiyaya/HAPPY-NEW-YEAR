b8c5237eecc27988511637c49e6b0e400116db95
Lakiyaya <Harithalaklidu001@gmail.com>
2025-12-31 18:30:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const wrapper = document.querySelector(".wrapper");
-const wish = document.getElementById("wish");
+const btn = document.getElementById("wishBtn");
+const messages = document.getElementById("messages");
+const msgArray = document.querySelectorAll(".msg");
 
-wrapper.addEventListener("click", () => {
-  wish.innerText = "එසේම වේවා 🙏";
+let index = 0;
+
+btn.addEventListener("click", () => {
+  // show messages one by one
+  if(index < msgArray.length){
+    msgArray[index].style.display = "block";
+    messages.classList.add("show");
+    index++;
+  } else {
+    // reset if clicked again
+    msgArray.forEach(msg => msg.style.display = "none");
+    index = 0;
+    messages.classList.remove("show");
+  }
 });
