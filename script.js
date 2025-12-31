96f81c1cd0da48a045cd806fc7a909e830ed35d6
Lakiyaya <Harithalaklidu001@gmail.com>
2025-12-31 18:14:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper");
+const wish = document.getElementById("wish");
+
+wrapper.addEventListener("click", () => {
+  wish.innerText = "එසේම වේවා 🙏";
+});
